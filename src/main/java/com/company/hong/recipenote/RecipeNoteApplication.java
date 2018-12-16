7d4a905bfedb54eae82e5349b20c1fa667c3a484
홍준호 <hongjunho@hongjunhoui-MacBook-Pro.local>
2018-12-16 14:36:09 +0900

    Configure init setting - gitignore - h2, handlebars setting - import.sql

---
+++ src/main/java/com/company/hong/recipenote/RecipeNoteApplication.java
@@ -0,0 +1,14 @@
+package com.company.hong.recipenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeNoteApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/company/hong/recipenote/RecipeNoteApplicationTests.java
@@ -0,0 +1,17 @@
+package com.company.hong.recipenote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeNoteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
